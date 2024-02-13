49dedd5a6a352a507cfb5687fc87d2f75b72b092
antonio-tresol <anthonny.badilla@ucr.ac.cr>
2024-02-12 18:14:52 -0600

    Added the main game screen, log, save and exit buttons. Increased the size of menu buttons.
    Co-Authored-By: Luis Solano <87628394+GoninDS@users.noreply.github.com>

--- src/RulesButton.gd
+++ src/RulesButton.gd
@@ -1,13 +1,5 @@
 extends Button
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _on_pressed():
 	OS.shell_open("https://en.wikipedia.org/wiki/Hexagonal_chess#Gli%C5%84ski's_hexagonal_chess")
 	pass # Replace with function body.
---
+++ src/SaveButton.gd
@@ -0,0 +1,8 @@
+extends Button
+
+func _on_pressed():
+	save_game()
+	
+func save_game():
+	print("Saved bro, trust me")
+	pass
